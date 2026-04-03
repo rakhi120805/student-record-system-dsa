bc809e659febf47cdd52aae254a8b2ca3600fd54
rakhi120508 <rakhikumarii7277@gmail.com>
2026-04-03 16:49:33 +0530

    Completed the project with basic features

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@ struct Student
     int marks;
     string section;
 };
+void swap(Student &a,Student &b){
+    Student temp=a;
+    a=b;
+    b=temp;
+}
 void add_students(Student arr[],int &count){
     if(count == 100){
         cout<<"The maximum capacity of this school is already achieved.";
@@ -27,22 +32,14 @@ void display_student_details(Student arr[],int &count){
     }
 }
 int search_student(Student arr[],int count,int roll){
-    bool found=false;
-    int st=-1;
+    
     for(int i=0;i<count;i++){
         if(roll==arr[i].roll_no){
-            found=true;
-            st=i;
-            break;
+            return i;
         }
     }
-    if(found==true){
-        cout<<arr[st].roll_no<<" "<<arr[st].name<<" "<<arr[st].section<<" "<<arr[st].marks<<endl;
-    }
-    else{
-        cout<<"Student not found";
-    }
-    return st;
+  
+    return -1;
 
 }
 void remove_student(Student arr[],int &count,int roll){
@@ -72,31 +69,132 @@ int topper(Student arr[],int count){
     }
     return maxi;
 }
+void update_details(Student arr[],int count,int roll){
+    string newname;
+    int newroll;
+    int newmarks;
+    string newsection;
+    int index=search_student(arr,count,roll);
+    if(count == 0 || index==-1){
+        cout<<"No student found to update!!";
+        return;
+    }
+    cout<<"choose what you have to change"<<endl;
+    cout<<"1: To change the name"<<endl;
+    cout<<"2: To change the roll number"<<endl;
+    cout<<"3: To change the section"<<endl;
+    cout<<"4: To change the marks"<<endl;
 
+    int option;
+    cin>>option;
+    switch (option)
+    {
+    case (1):
+        cout<<"Enter the new name: ";
+        cin>>newname;
+        arr[index].name=newname;
+        break;
+    case (2):
+        cout<<"Enter the new roll number: ";
+        cin>>newroll;
+        arr[index].roll_no=newroll;
+        break;
+    case (3):
+        cout<<"Enter the new section: ";
+        cin>>newsection;
+        arr[index].section=newsection;
+        break;
+    case (4):
+        cout<<"Enter the new marks: ";
+        cin>>newmarks;
+        arr[index].marks=newmarks;
+        break;
+    
+    default:
+        cout<<"Invalid choice";
+        break;
+    }
+
+}
+
+void sort_students_by_marks(Student arr[],int count){
+    for(int i=count;i>0;i--){
+        for(int j=0;j<i-1;j++){
+            if(arr[j].marks>arr[j+1].marks){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+
+}
+
+void print_menu(){
+    cout<<"Please choose the below options: "<<endl;
+    cout<<"1-> Add a new student."<<endl;
+    cout<<"2-> Display the details of the students of the school."<<endl;\
+    cout<<"3-> Search a student."<<endl;
+    cout<<"4-> Remove a student details. "<<endl;
+    cout<<"5-> Know the topper's mark. "<<endl;
+    cout<<"6-> Update a student details. "<<endl;
+    cout<<"7-> sort the student details. "<<endl;
+    cout<<"0-> Exit. "<<endl;
+}
 
 int main(){
     int count=0;
     int n=100;
     Student arr[n];
-    int a,b,c,d,e;
-    int options;
-    cout<<"Please choose the below options: "<<endl;
-    cout<<"a-> Add a new student "<<endl;
-    cout<<"b-> Display the details of the students of the school"<<endl;\
-    cout<<"c-> Search a student "<<endl;
-    cout<<"d->  "<<endl;
+
+    int choice;
+    int roll;
+    int ind;
     
+    do{ 
+        print_menu();
+        cin>>choice;
+        switch (choice)
+        {
+        case (1):
+            add_students(arr,count);
+            break;
+        case (2):
+            display_student_details(arr,count);
+            break;
+        case (3):
+            
+            cout<<"Enter the roll number: ";
+            cin>>roll;
+            ind=search_student(arr,count,roll);
+            cout<<arr[ind].name<<" "<<arr[ind].roll_no<<" "<<arr[ind].section<<" "<<arr[ind].marks<<endl;
+            break;
+        case (4):
+            cout<<"Enter the roll number: ";
+            cin>>roll;
+            remove_student(arr,count,roll);
+            break;
+        case (5):
+            ind=topper(arr,count);
+            if(ind!=-1)cout<<arr[ind].name<<" : "<<arr[ind].marks<<endl;
+            else cout<<"Topper not exists";
+            break;
+        case (6):
+            cout<<"Enter the roll number: ";
+            cin>>roll;
+            update_details(arr,count,roll);
+            break;
+        case (7):
+            sort_students_by_marks(arr,count);
+            break;
+        case (0):
+            cout<<"exiting ... ";
+            break;
+        default:
+            cout<<"Invalid choice";
+            break;
+        }
 
-    cin>>options;
-    if(options==a){
-        add_students(arr,count);
     }
-    else if(options==b){
-        display_student_details(arr,count);
-    }
-    else if(options==c){
-
-    }
+    while(choice!=0);
     
 
 
